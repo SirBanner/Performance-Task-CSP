874ad9b4df95f13f3bb8aadef62c0feac1bc8ff2
SirBanner <82613781+SirBanner@users.noreply.github.com>
2021-04-27 13:58:50 -0700

    Update Basic Word Function.py

--- Basic Word Function.py
+++ Basic Word Function.py
@@ -1,14 +1,29 @@
-from words import listofwords
+from words import wordlist
 import random
 
 
 def get_word():
-  word = random.choice(listofwords)
-  return word.upper()
+    word = random.choice(wordlist)
+    return word.upper()
+
 
 def play(word):
   word_complettion = "_" * len(word)
   guessed = False
-  guessed_letters =[]
+  guessed_letters = []
   guessed_words = []
-  tries = 5
+  tries = 6
+  print("Start Hangman.")
+  print(display_hangman(tries))
+  print(word_complettion)
+  print("\n")
+ while not guessed and tries > 0:
+   guess = input("Guess a letter or word: ").upper()
+   if len(guess) == 1 and guess.isalpha():
+
+     elif len(guess) == len(word) and guess.isalpha():
+      
+    else:
+      pring("Not Valid Guess")
+    print(display_hangman(tries))
+    print("\n")
